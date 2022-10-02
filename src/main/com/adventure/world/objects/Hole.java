2cb411b268c002afee341252601b8573a3165dc8
KPjustCoding <pasowiczk@gmail.com>
2022-10-02 11:28:13 -0500

    Progressed on Mastery, still receiving some errors

--- src/main/com/adventure/world/objects/Hole.java
+++ src/main/com/adventure/world/objects/Hole.java
@@ -9,30 +9,35 @@ public class Hole {
      * @param content - the item that is covered by the hole.
      */
     public Hole(Key content) {
-        HoleContent holeContent = new HoleContent(content);
+       this.holeContent = new HoleContent(content);
 
     }
 
     public boolean isCovered() {
         //TODO This value should come from HoleContent
-        return null;
+        return this.holeContent.isCovered();
+    }
 
     /**
      * Uncovers the hole. If applicable, the contents are now revealed.
      */
     public void dig() {
         //TODO this function should update HoleContent's isCovered property.
+        holeContent.setIsCovered(false);
 
     }
 
     /**
      * If the HoleContent is uncovered, return the key. Otherwise, return null.
-     * @return the key if the hold is uncovered.
+     * @return the key if the hole is uncovered.
      */
     private Key getKeyIfPossible() {
+        if (!this.holeContent.isCovered())
+            return this.holeContent.getKey();
+        else return null;
+        }
         //TODO Get the HoleContent's content only if the hole is uncovered.
-        return null;
-    }
+
 
     /**
      * If the HoleContent is uncovered, return the content. Otherwise, return null.
@@ -40,5 +45,6 @@ public class Hole {
      */
     public Tangible getContent() {
         return getKeyIfPossible();
+
     }
 }
--- src/main/com/adventure/world/objects/HoleContent.java
+++ src/main/com/adventure/world/objects/HoleContent.java
@@ -12,11 +12,11 @@ public class HoleContent {
     }
 
     public void setIsCovered(boolean isCovered){
-
+        this.isCovered = isCovered;
     }
 
     public boolean isCovered(){
-        return this.isCovered();
+        return this.isCovered;
     }
 
     public Key getKey() {
